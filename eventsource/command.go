afbbae7a0124ce432509da1596266d5edd9fdf8c
Mitchell Rodrigues <mitchell.rodrigues@therealreal.com>
2023-09-16 20:16:23 -0500

    Missed a couple of spots, still calling repo

--- eventsource/command.go
+++ eventsource/command.go
@@ -15,7 +15,7 @@ type CommandValidator interface {
 }
 
 func Call(ctx golly.Context, ag Aggregate, cmd Command, metadata Metadata) error {
-	repo := ag.Repo(ctx)
+	repo := Repo(ctx, ag)
 
 	if validator, ok := cmd.(CommandValidator); ok {
 		if err := validator.Validate(ctx, ag); err != nil {
@@ -33,7 +33,7 @@ func Call(ctx golly.Context, ag Aggregate, cmd Command, metadata Metadata) error
 }
 
 func LoadIfNotNew(ctx golly.Context, ag Aggregate) error {
-	repo := ag.Repo(ctx)
+	repo := Repo(ctx, ag)
 
 	if !repo.IsNewRecord(ag) {
 		if err := repo.Load(ctx, ag); err != nil {
@@ -43,14 +43,20 @@ func LoadIfNotNew(ctx golly.Context, ag Aggregate) error {
 	return nil
 }
 
-// Execute executes the command, ensuring that events are saved to the backend before in-memory processing.
-func Execute(ctx golly.Context, ag Aggregate, cmd Command, metadata Metadata) error {
+func Repo(ctx golly.Context, ag Aggregate) Repository {
 	// Tired of not being able to stub this - so now we can (This still doesnt fully help if someone runs)
 	// something within their command that calls ag.Repo(ctx) - but it's a start
-	repo := repoFromContext(ctx)
-	if repo == nil {
-		repo = ag.Repo(ctx)
+	if repo := repoFromContext(ctx); repo != nil {
+		return repo
 	}
+
+	return ag.Repo(ctx)
+
+}
+
+// Execute executes the command, ensuring that events are saved to the backend before in-memory processing.
+func Execute(ctx golly.Context, ag Aggregate, cmd Command, metadata Metadata) error {
+	repo := Repo(ctx, ag)
 
 	// Perform the given command on the aggregate
 	if err := cmd.Perform(ctx, ag); err != nil {
