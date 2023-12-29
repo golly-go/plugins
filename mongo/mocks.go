a74866498a8502fa1fdc822c3d715f5ea2f383a6
Mitchell Rodrigues <mitchell.rodrigues@therealreal.com>
2023-12-29 13:16:55 -0800

    Make sure to implement the full stuff

--- mongo/mocks.go
+++ mongo/mocks.go
@@ -1,6 +1,8 @@
 package mongo
 
 import (
+	"time"
+
 	"github.com/golly-go/golly"
 	"github.com/stretchr/testify/mock"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -8,14 +10,13 @@ import (
 )
 
 type MockClient struct {
-	mock *mock.Mock
+	mock.Mock
 
 	MockCollection *MockCollection
 }
 
 func NewMockClient(ctx golly.Context) (golly.Context, *MockClient) {
 	client := MockClient{
-		mock:           &mock.Mock{},
 		MockCollection: &MockCollection{},
 	}
 
@@ -25,17 +26,54 @@ func NewMockClient(ctx golly.Context) (golly.Context, *MockClient) {
 }
 
 // Collection provides a mock function with given fields: obj
-func (_m *MockClient) Collection(obj interface{}) Collection {
+func (_m *MockClient) Collection(golly.Context, interface{}) Collection {
 	return _m.MockCollection
 }
 
+// Connect provides a mock function with given fields: ctx
+func (_m *MockClient) Connect(ctx golly.Context) error {
+	ret := _m.Called(ctx)
+
+	return ret.Error(0)
+}
+
+// Disconnect provides a mock function with given fields: ctx
+func (_m *MockClient) Disconnect(ctx golly.Context) error {
+	ret := _m.Called(ctx)
+	return ret.Error(0)
+}
+
+func (_m *MockClient) IsConnected(ctx golly.Context) bool {
+	_m.Called(ctx)
+
+	return true
+}
+
+// Ping provides a mock function with given fields: ctx, timeout
+func (_m *MockClient) Ping(ctx golly.Context, timeout ...time.Duration) error {
+	ret := _m.Called(ctx, timeout)
+	return ret.Error(0)
+}
+
+func (_m *MockClient) Transaction(ctx golly.Context, fn func(ctx golly.Context) error) error {
+	ret := _m.Called(ctx, fn)
+	return ret.Error(0)
+}
+
+// Database provides a mock function with given fields: ctx, options
+func (_m *MockClient) Database(ctx golly.Context, options DatabaseOptions) Client {
+	return _m
+}
+
+var _ Client = (*MockClient)(nil)
+
 type MockCollection struct {
-	mock *mock.Mock
+	mock.Mock
 }
 
 // Find provides a mock function with given fields: out, filter, options
 func (_m *MockCollection) Find(out interface{}, filter interface{}, options ...*options.FindOptions) error {
-	ret := _m.mock.Called(out, filter, options)
+	ret := _m.Called(out, filter, options)
 
 	var r0 error
 	if rf, ok := ret.Get(0).(FinderFunc); ok {
@@ -49,7 +87,7 @@ func (_m *MockCollection) Find(out interface{}, filter interface{}, options ...*
 
 // FindOne provides a mock function with given fields: out, filter
 func (_m *MockCollection) FindOne(out interface{}, filter interface{}) error {
-	ret := _m.mock.Called(out, filter)
+	ret := _m.Called(out, filter)
 
 	var r0 error
 	if rf, ok := ret.Get(0).(func(interface{}, interface{}) error); ok {
@@ -63,7 +101,7 @@ func (_m *MockCollection) FindOne(out interface{}, filter interface{}) error {
 
 // FindByID provides a mock function with given fields: out, id
 func (_m *MockCollection) FindByID(out interface{}, id interface{}) error {
-	ret := _m.mock.Called(out, id)
+	ret := _m.Called(out, id)
 
 	var r0 error
 	if rf, ok := ret.Get(0).(func(interface{}, interface{}) error); ok {
@@ -77,7 +115,7 @@ func (_m *MockCollection) FindByID(out interface{}, id interface{}) error {
 
 // Insert provides a mock function with given fields: out
 func (_m *MockCollection) Insert(out interface{}) error {
-	ret := _m.mock.Called(out)
+	ret := _m.Called(out)
 
 	var r0 error
 	if rf, ok := ret.Get(0).(func(interface{}) error); ok {
@@ -91,7 +129,7 @@ func (_m *MockCollection) Insert(out interface{}) error {
 
 // UpdateOne provides a mock function with given fields: out, updateDocument
 func (_m *MockCollection) UpdateOne(out interface{}, updateDocument interface{}) error {
-	ret := _m.mock.Called(out, updateDocument)
+	ret := _m.Called(out, updateDocument)
 
 	var r0 error
 	if rf, ok := ret.Get(0).(func(interface{}, interface{}) error); ok {
@@ -105,7 +143,7 @@ func (_m *MockCollection) UpdateOne(out interface{}, updateDocument interface{})
 
 // UpdateOneDocument provides a mock function with given fields: out
 func (_m *MockCollection) UpdateOneDocument(out interface{}) error {
-	ret := _m.mock.Called(out)
+	ret := _m.Called(out)
 
 	var r0 error
 	if rf, ok := ret.Get(0).(func(interface{}) error); ok {
@@ -118,7 +156,7 @@ func (_m *MockCollection) UpdateOneDocument(out interface{}) error {
 }
 
 func (_m *MockCollection) C() *mongo.Collection {
-	_m.mock.Called()
+	_m.Called()
 
 	return &mongo.Collection{}
 }
